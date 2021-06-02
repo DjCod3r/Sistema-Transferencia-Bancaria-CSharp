eaf0bce25f868a59d3d4af044d5f39b9d5fa5e98
DjCod3r <djonatanschvambach@gmail.com>
2021-06-01 21:06:26 -0300

    Criação do Projeto de Sistema de  transferencia bancaria

---
+++ App Transf Bancaria/Conta.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Transf_Bancaria
+{
+    class Conta
+    {
+        private TipoConta TipoConta { get; set; }
+        private string NOME { get; set; }
+        public int ID { get;private set; }
+        private double SALDO { get; set; }
+        private double CREDITO { get; set; }
+
+        public Conta(TipoConta tipoConta, string nome, int id, double saldo, double credito)
+        {
+            TipoConta = tipoConta;
+            NOME = nome;
+            ID = id;
+            SALDO = saldo;
+            CREDITO = credito;
+        }
+
+
+        public bool Sacar(double valorSaque)
+        {
+            if(SALDO - valorSaque < (CREDITO * -1))
+            {
+                Console.WriteLine("Saldo Insuficiente!");
+                return false;
+            }
+            SALDO -= valorSaque;
+            Console.WriteLine($"Titular da Conta: {NOME} , Saldo atual: {SALDO}");
+
+            return true;
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            SALDO += valorDeposito;
+            Console.WriteLine($"Titular da Conta: {NOME} , Saldo atual: {SALDO}");
+        }
+
+        public void Transferencia(double valorTransferencia, Conta destinatario)
+        {
+            if (Sacar(valorTransferencia))
+            {
+                destinatario.Depositar(valorTransferencia);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Conta: {ID} , Titular: {NOME}, Saldo: {SALDO}, Creditos: {CREDITO} ";
+        }
+        
+       
+    }
+}
---
+++ App Transf Bancaria/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App_Transf_Bancaria
+{
+    class Program
+    {
+        static List<Conta> listaContas = new List<Conta>();
+        static void Main(string[] args)
+        {
+            string menuUsuario = Menu();
+
+            while (menuUsuario.ToUpper() != "X")
+            {
+                switch (menuUsuario)
+                {
+                    case "1":
+                        ListaDeContas();
+                        break;
+                    case "2":
+                        InserirConta();
+                        break;
+                    case "3":
+                        Transferencia();
+                        break;
+                    case "4":
+                        Sacar();
+                        break;
+                    case "5":
+                        Depositar();
+                        break;
+                    case "L":
+                        Console.Clear();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+
+                }
+                menuUsuario = Menu();
+            }
+            Console.WriteLine("Obrigado por utilizar nossos serviços");
+            Console.WriteLine("Pressione qualquer tecla para sair da Aplicação");
+            Console.ReadKey();
+        }
+
+        private static void Transferencia()
+        {
+            Console.Write("Qual o indice da conta origem ? ");
+            int indice = int.Parse(Console.ReadLine());
+
+            Console.Write("Qual o indice da conta do destinatario ? ");
+            int indiceDest = int.Parse(Console.ReadLine());
+
+            Console.Write("Valor da transferencia ");
+            double transferencia = double.Parse(Console.ReadLine());
+
+            listaContas[indice].Transferencia(transferencia, listaContas[indiceDest]);
+
+        }
+        private static void Sacar()
+        {
+            Console.WriteLine("Saque");
+            Console.WriteLine("Digite o Indice da Conta");
+            int nConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Valor para Saque : ");
+            double saq = double.Parse(Console.ReadLine());
+
+            listaContas[nConta].Sacar(saq);
+           
+        }
+        private static void Depositar()
+        {
+            Console.WriteLine("Deposito");
+            Console.WriteLine("Digite o Indice da Conta");
+            int nConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Valor para Deposito : ");
+            double saq = double.Parse(Console.ReadLine());
+
+            listaContas[nConta].Depositar(saq);
+
+        }
+        private static void InserirConta()
+        {
+            Console.WriteLine("Digite 1 para pessoa Fisica ou 2 para pessoa Juridica");
+            int tipoC = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Nome do Cliente : ");
+            string nomeTitular = Console.ReadLine();
+
+            Console.Write("ID da Conta : ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor do saldo inicial : ");
+            double saldo = double.Parse(Console.ReadLine());
+
+            Console.Write("Valor do Credito Especial : ");
+            double credito = double.Parse(Console.ReadLine());
+
+            Conta novaConta = new Conta((TipoConta)tipoC, nomeTitular, id, saldo, credito);
+
+            listaContas.Add(novaConta);
+        }
+
+
+        private static void ListaDeContas()
+        {
+            Console.WriteLine("Lista de Contas");
+            Console.WriteLine();
+            
+            if(listaContas.Count == 0)
+            {
+                Console.WriteLine("Ainda não existe nenhuma conta cadastrada");
+                return;
+            }
+
+            for(int i =0; i < listaContas.Count; i++)
+            {
+                Conta conta = listaContas[i];
+                Console.WriteLine($"{i} {conta} " );
+            }
+
+
+        }
+        public static string Menu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Que operação deseja realizar ?");
+            Console.WriteLine();
+            Console.WriteLine("1 - Lista de Contas");
+            Console.WriteLine("2 - Inserir uma nova Conta");
+            Console.WriteLine("3 - Realizar Trasnferencia Bancaria");
+            Console.WriteLine("4 - Saque");
+            Console.WriteLine("5 - Deposito");
+            Console.WriteLine("L - Limpar tela ");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string menuUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return menuUsuario;
+        }
+    }
+}
